753cbb4dce8c10b352231c781b6d8d035a5df818
Juaness06 <jesteban.granada@javeriana.edu.co>
2024-02-15 17:18:18 -0500

    comit con cambios

---
+++ Código Fuente/demo/src/main/java/com/example/demo/Entidad/Perro.java
@@ -0,0 +1,11 @@
+package com.example.demo.Entidad;
+
+public class Perro {
+    
+    private String nombre;
+    private String raza;
+    private String color;
+    private String sexo;
+    private String tamanio;
+    private String fechaNacimiento;
+}
