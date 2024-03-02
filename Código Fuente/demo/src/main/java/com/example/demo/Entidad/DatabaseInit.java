bb2b216623344fd7f8e9dc2218230eb7a9bc7b0e
Nikoquin1 <quintana.nicolas@javeriana.edu.co>
2024-03-01 21:41:32 -0500

    listo los 100 valores

--- Código Fuente/demo/src/main/java/com/example/demo/Entidad/DatabaseInit.java
+++ Código Fuente/demo/src/main/java/com/example/demo/Entidad/DatabaseInit.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
-
+import java.util.concurrent.ThreadLocalRandom;
 import com.example.demo.Repositorio.ReporsitorioCliente;
 import com.example.demo.Repositorio.RepositorioPerro;
 
@@ -24,25 +24,77 @@ public class DatabaseInit implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        repo.save(new Cliente("Jairo","jairo.man@gamilcom",32416391,"jairo.man","123"));
-        repo.save(new Cliente("Esteban","esteban.granada@gmail.com",35687654,"Grandala","222"));
-        repo.save(new Cliente("Santiago", "sanatigo.paez@gmail.com",32176545,"Killercoolyeah","444"));
-        repo.save(new Cliente("Juana","juana.suaracost@gmail.com",35687654,"Suaracost","555"));
+            // Generar y guardar 50 clientes con detalles más realistas
+            String[] nombresClientes = {"Ana", "Beatriz", "Carlos", "David", "Elena", "Fernando", "Gabriela", "Héctor", "Irene", "Jorge","Alejandro", "Bruno", "Daniel", "Eduardo", "Francisco",
+            "Guillermo", "Hugo", "Íñigo", "Joaquín", "Leonardo",
+            "Manuel", "Nicolás", "Óscar", "Pablo", "Quentin",
+            "Ricardo", "Sergio", "Tomás", "Víctor", "Xavier","Alicia", "Beatriz", "Carmen", "Diana", "Estefanía",
+            "Fernanda", "Gloria", "Helena", "Isabel", "Julia",
+            "Karen", "Laura", "Marta", "Natalia", "Olivia",
+            "Patricia", "Raquel", "Sonia", "Teresa", "Úrsula",
+            "Valeria", "Wendy", "Ximena", "Yolanda", "Zoe"};
 
-        repo2.save(new Perro("https://media.istockphoto.com/id/175221593/es/foto/linda-poco-perro-tejonero.jpg?s=1024x1024&w=is&k=20&c=wf0sWuGn-m4UugX5wouI4CA52XQLz3WkeCN1_Q69QBI=", "Firuslai", 001, "Salchicha", 7, true, 9.2, 2));
-        repo2.save(new Perro("https://media.istockphoto.com/id/509052128/es/foto/oro-retriever-sentado-en-frente-de-un-fondo-blanco.jpg?s=1024x1024&w=is&k=20&c=C4xUiJIirtS9I8fjkLjub7TSKv1wx1n2vYauNQ6JzZ4=", "Buddy", 002, "Golden Retriever", 5, false, 30.0, 1));
-        repo2.save(new Perro("https://media.istockphoto.com/id/168620477/es/foto/retrato-de-un-bulldog-ingl%C3%A9s.jpg?s=1024x1024&w=is&k=20&c=4hilsmilH3RfZ3iAokkbvPwBE8CC30KlW_6dPMWyY6M=", "Rocky", 004, "Bulldog", 8, true, 23.5, 5));
-        repo2.save(new Perro("https://www.dailypaws.com/thmb/ZBHY6XQwtFPrrYGMG2wA4JZLKgg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/american-bulldog-running-177755054-2000-6243a6aadc4b47c0bb892363976310ce.jpg", "Max", 005, "American-Bulldog", 4, true, 15.0, 5));
-        repo2.save(new Perro("https://media.istockphoto.com/id/950511940/es/foto/shisu-perro-sobre-fondo-blanco.jpg?s=1024x1024&w=is&k=20&c=wGzFvjZo2YtpfPLOsl3wR4fj9auZ0YzvStwdJ23nwXc=", "Milo", 006, "Shi Tzu", 7, true, 7.2, 2));
-        repo2.save(new Perro("https://media.istockphoto.com/id/167229347/es/foto/cachorro.jpg?s=1024x1024&w=is&k=20&c=q-GTm-vPtKo1RYlUebwIxrLdeyHYRWnoHLGUgpt79zg=", "Bella", 007, "Pomeranian", 5, false, 3.5, 1));
-        repo2.save(new Perro("https://media.istockphoto.com/id/500851439/es/foto/d%C3%A1lmata-aislado-en-blanco.jpg?s=1024x1024&w=is&k=20&c=GR_6JtLDwT7XOtr6q_H4fvdqyNC_zN9P57AM9UcQpUc=", "Spot", 010, "Dalmatian", 7, true, 25.0, 2));
-
-        Cliente asociar = repo.findById(1L).get();
-        for (Perro perro : repo2.findAll()) {
-            perro.setCliente(asociar);
-            repo2.save(perro);
+            String[] apellidos = {"González", "Rodríguez", "Martínez", "Sánchez", "Pérez", "López", "García", "Díaz", "Vásquez", "Jiménez","Álvarez", "Benítez", "Camacho", "Domínguez", "Espinoza",
+            "Fuentes", "Gutiérrez", "Hernández", "Ibarra", "Juárez",
+            "Klein", "Lugo", "Mendoza", "Navarro", "Ochoa",
+            "Paredes", "Quiroz", "Reyes", "Soto", "Torres",
+            "Ureña", "Varela", "Zamora", "Quiñones", "Yáñez",
+            "Zúñiga", "Aguirre", "Barrera", "Casanova", "Díaz",};
+            String[] apodos = {
+                "Lobo", "Doc", "Chispa", "Roca", "Príncipe", "Reina", "Azul", "Oso", "Halcón",
+                "Fantasma", "Tigre", "Blaze", "Ace", "Shadow", "Gadget", "Flash", "Wolf", "Duke",
+                "Vikingo", "Pirata", "Ninja", "Samurai", "Wizard", "Spartan", "Phantom", "Ranger",
+                "Cazador", "Jazz", "King", "Knight", "Lightning", "Maverick", "Nomad", "Phoenix",
+                "Quantum", "Rocket", "Saber", "Tank", "Vortex", "Zen", "Archer", "Bolt", "Cyborg",
+                "Drift", "Echo", "Flame", "Glitch", "Hawk", "Iron", "Jester"
+            };
+            
+            for (int i = 1; i <= 50; i++) {
+                String nombre = nombresClientes[i % nombresClientes.length] + " " + apellidos[i % apellidos.length];
+                String email = nombresClientes[i % nombresClientes.length] + apellidos[i % apellidos.length] + i + "@ejemplo.com";
+                long telefono = 30000000L + i;
+                String usuario = apodos[i % apodos.length] + i;
+                String contraseña =  "" + i;
+                
+                Cliente cliente = new Cliente(nombre, email, telefono, usuario, contraseña);
+                repo.save(cliente);
+            }
+        
+            // Generar y guardar 50 perros con detalles más realistas
+            String[] nombresPerros = {
+                "Max", "Bella", "Charlie", "Luna", "Rocky", "Molly", "Toby", "Lucy", "Coco", "Bailey",
+                "Daisy", "Oliver", "Sadie", "Maggie", "Buddy", "Oscar", "Lola", "Winston", "Ruby", "Bear",
+                "Duke", "Penny", "Zoe", "Jack", "Ellie", "Riley", "Dexter", "Rosie", "Louie", "Mia",
+                "Baxter", "Stella", "Gus", "Ivy", "Murphy", "Piper", "Leo", "Lily", "Benny", "Sophie",
+                "Harley", "Milo", "Jasper", "Emma", "Tucker", "Ziggy", "Hugo", "Lulu", "Brady", "Lady",
+                "Sam", "Fiona", "Otis", "Chloe", "Marley", "Scout", "Gigi", "Ollie", "Gracie", "George"
+            };
+            String[] razas = {"Labrador", "Bulldog", "Beagle", "Poodle", "Boxer", "Dálmata", "Pastor Alemán", "Golden Retriever", "Husky Siberiano", "Chihuahua"};
+            
+            for (int i = 1; i <= 50; i++) {
+                String urlImagen = "URL de imagen genérica aquí";
+                String nombre = nombresPerros[i % nombresPerros.length];
+                int id = i; 
+                String raza = razas[i % razas.length];
+                int edad = ThreadLocalRandom.current().nextInt(1, 16);;
+                boolean sexo = i % 2 == 0;
+                double pesoRaw = 5.0 + (40.0 - 5.0) * ThreadLocalRandom.current().nextDouble();
+                double peso = Math.round(pesoRaw * 10) / 10.0;
+                int energia = (i % 3) + 1; 
+                
+                Perro perro = new Perro(urlImagen, nombre, id, raza, edad, sexo, peso, energia);
+                repo2.save(perro);
+            }
+        
+            // Asociar todos los perros al primer cliente como ejemplo
+            Cliente asociar = repo.findById(1L).orElse(null);
+            if (asociar != null) {
+                for (Perro perro : repo2.findAll()) {
+                    perro.setCliente(asociar);
+                    repo2.save(perro);
+                }
+            }
         }
-    }
 
                 
 
